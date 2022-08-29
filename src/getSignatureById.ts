d1168d15c6c1761d4f8e4f5a4afcb6c82d2896b7
Bhawick Jain <bhawick@gmail.com>
2022-08-29 17:31:37 +0100

    implemetn an example of sql injection vulnerable code

--- src/getSignatureById.ts
+++ src/getSignatureById.ts
@@ -1,9 +1,14 @@
 import { Client } from "pg";
 
 export async function getSignatureById(id: string, client: Client): Promise<any[] | null> {
-    const query = 'SELECT * FROM signatures WHERE id = $1'
-    const value = [`${id}`]
-    const {rows} = await client.query(query, value)
+    // sql injection vulnerable code
+    const query = `SELECT * FROM signatures WHERE id = ${id}`
+
+    // using the (text, values) postgres SQL library has a 'battle tested'
+    // parameter substitution code which detects alterations to the over
+    // all query given an input.
+    // So it is not a naive substitution!
+    const {rows} = await client.query(query)
     
     return rows.length > 0 ? rows : null
 }
